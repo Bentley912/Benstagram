8694c3377d40b5aad56819f007281110471c760c
Patrick Bentley <pbentley07@gmail.com>
2016-08-27 15:27:00 -0400

    Added Form for Pic Upload, Title and Description

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,17 @@
 class PicsController < ApplicationController
-
   def index
   end
-  
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+  end
+  private
+
+  def pic_params
+    Params.require(:pic).permit(:title,:description)
+  end
 end
